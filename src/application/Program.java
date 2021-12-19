927eb7d1a8cfce12590c3db36025e7a4b59e98eb
avelinovinicius <vinicius.avelino@mercadolivre.com>
2021-12-19 18:08:31 -0300

    Exercicio 3 - prototipo supermercado

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Quantos produtos? ");
+		int n = sc.nextInt();
+
+		Product[] vect = new Product[n];
+
+		for (int i = 0; i < n; i++) {
+			sc.nextLine();
+			System.out.println("Produto: ");
+			String name = sc.nextLine();
+
+			System.out.println("Preco: ");
+			double price = sc.nextDouble();
+
+			System.out.println("Quantidade: ");
+			int quantity = sc.nextInt();
+
+			vect[i] = new Product(name, quantity, price);
+
+		}
+
+		// Calculo e exibicao do Array
+		double sum = 0.0;
+		for (int i = 0; i < n; i++) {
+			sum += vect[i].getPrice() * vect[i].getQuantity();
+
+			System.out.println("Produto " + (i + 1) + "\n" + vect[i].getName() + "\n" + "R$ " + vect[i].getPrice()
+					+ "\n" + "Quantidade " + vect[i].getQuantity() + "\n");
+
+		}
+		System.out.println("Valor total: R$ " + sum);
+
+		sc.close();
+	}
+
+}
